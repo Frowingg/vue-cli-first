36eda17084473321b5a4400e9c1864d67d3c1364
Frowingg <edbico0@gmail.com>
2022-07-05 17:19:18 +0200

    come da lezione

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,24 @@
+<!-- HTML -->
+<template>
+
+    <div class="heading">
+        <h1>Il mio titolo</h1>
+    </div>
+
+</template>
+
+<!-- JS -->
+<script>
+
+export default{
+    name: 'MyTitle',
+}
+
+</script>
+
+<!-- CSS -->
+<style lang="scss" scoped>
+    h1{
+        color: red;
+    }
+</style>
